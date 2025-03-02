4c7815dab67c9857b4f2c81122a2713e97c0ce06
Jijo <jijojoshy372@gmail.com>
2025-03-02 10:57:07 +0530

    finalize commit

--- guest/urls.py
+++ guest/urls.py
@@ -3,11 +3,11 @@ from guest import views
 app_name="webguest"
 
 urlpatterns = [
-    path('',views.log,name="Login"),
+    path('log/',views.log,name="Login"),
     path('usrr/',views.usr,name="Userreg"),
     path('ajaxplace/',views.AjaxPlace,name="Ajax-Place"),
     path('ownrr/',views.ownr,name="ownrreg"),
-    path('home',views.home,name="homepage"),
+    path('',views.home,name="homepage"),
     path('fpass/', views.ForgetPassword,name="forpass"),
     path('otpver/', views.OtpVerification,name="verification"),
     path('create/', views.CreateNewPass,name="create"),
